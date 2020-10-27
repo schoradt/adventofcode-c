6b6047b11c5d9153d3643e723d81789163cc7933
Sven Schoradt <sven.schoradt@googlemail.com>
2020-10-27 22:10:12 +0100

    ADD: main runner and aoc2019 creator

---
+++ src/Runner.cpp
@@ -0,0 +1,19 @@
+
+#include "Day1.h"
+
+#include <iostream>
+
+using namespace std;
+
+void day1() {
+    Day1 day1;
+
+    cout << "Day 1 part 1 " << day1.test() << endl;
+    cout << "Day 1 part 2 " << day1.test() << endl;
+}
+
+int main() {
+    day1();
+
+    return 0;
+}
