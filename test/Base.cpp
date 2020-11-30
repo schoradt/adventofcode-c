0398c56dc9dbb1225e66b52f4a781b6a2d34994a
Sven Schoradt <sven.schoradt@googlemail.com>
2020-11-30 23:35:38 +0100

    FIX: cherry pick base changes fro 2019 branch and add tests and fixes

--- test/Base.cpp
+++ test/Base.cpp
@@ -35,6 +35,15 @@ TEST_CASE("Testing Base loadLines")
     CHECK_MESSAGE("5" == res[4], "res pos 4");
 }
 
+TEST_CASE("Testing Base loadLine")
+{
+    Base base;
+
+    string res = base.loadLine("../data/test2.txt");
+
+    CHECK_MESSAGE(res.compare("1,2,3,4,5") == 0, "line loaded");
+}
+
 TEST_CASE("Testing Base loadLinesString")
 {
     Base base;
@@ -71,4 +80,40 @@ TEST_CASE("Testing Base parseIntegerLines")
     CHECK_MESSAGE(3 == res[2], "res pos 2");
     CHECK_MESSAGE(4 == res[3], "res pos 3");
     CHECK_MESSAGE(5 == res[4], "res pos 4");
+}
+
+TEST_CASE("Testing Base joinLine strings")
+{
+    Base base;
+
+    vector<string> res = base.splitLine("1,2,3,4,5");
+
+    CHECK_MESSAGE(5 == res.size(), "res size is 5");
+
+    CHECK_MESSAGE("1" == res[0], "res pos 0");
+    CHECK_MESSAGE("2" == res[1], "res pos 1");
+    CHECK_MESSAGE("3" == res[2], "res pos 2");
+    CHECK_MESSAGE("4" == res[3], "res pos 3");
+    CHECK_MESSAGE("5" == res[4], "res pos 4");
+
+    CHECK_MESSAGE(base.joinLine(res).compare("1,2,3,4,5") == 0, "join matches");
+}
+
+TEST_CASE("Testing Base joinLine integer")
+{
+    Base base;
+
+    vector<string> strings = base.splitLine("1,2,3,4,5");
+
+    vector<int> res = base.parseIntergerLines(strings);
+
+    CHECK_MESSAGE(5 == res.size(), "res size is 5");
+
+    CHECK_MESSAGE(1 == res[0], "res pos 0");
+    CHECK_MESSAGE(2 == res[1], "res pos 1");
+    CHECK_MESSAGE(3 == res[2], "res pos 2");
+    CHECK_MESSAGE(4 == res[3], "res pos 3");
+    CHECK_MESSAGE(5 == res[4], "res pos 4");
+
+    CHECK_MESSAGE(base.joinLine(res).compare("1,2,3,4,5") == 0, "join matches");
 }
