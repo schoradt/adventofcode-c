94037b107b2f66808605fecd4eaec2bb0b2ff0b2
Sven Schoradt <sven.schoradt@googlemail.com>
2020-12-03 08:19:11 +0100

    FIX: day 3 part 1

--- src/Day3.cpp
+++ src/Day3.cpp
@@ -10,8 +10,23 @@ Day3::Day3() {
 }
 
 int Day3::process1(vector<string> input) {
+    size_t x = 0;
+    size_t y = 0;
 
-    return -1;
+    int length = input.at(0).size();
+
+    int trees = 0;
+
+    while (y < input.size()) {
+        if (input.at(y).at(x) == '#') {
+            trees++;
+        }
+
+        x = (x + 3) % length;
+        y = y + 1;
+    }
+
+    return trees;
 }
 
 int Day3::process2(vector<string> input) {
--- test/Day3.cpp
+++ test/Day3.cpp
@@ -8,16 +8,16 @@ TEST_CASE("Testing Day 2")
 {
     Day3 day;
 
-    vector<string> input = day.loadLinesString("1-3 a: abcde\n1-3 b: cdefg\n2-9 c: ccccccccc");
+    vector<string> input = day.loadLinesString("..##.......\n#...#...#..\n.#....#..#.\n..#.#...#.#\n.#...##..#.\n..#.##.....\n.#.#.#....#\n.#........#\n#.##...#...\n#...##....#\n.#..#...#.#");
 
-    CHECK_MESSAGE(-1 == day.process1(input), "test process 1");
+    CHECK_MESSAGE(7 == day.process1(input), "test process 1");
 }
 
 TEST_CASE("Testing Day 2 process 2")
 {
     Day3 day;
 
-    vector<string> input = day.loadLinesString("1-3 a: abcde\n1-3 b: cdefg\n2-9 c: ccccccccc");
+    vector<string> input = day.loadLinesString("..##.......\n#...#...#..\n.#....#..#.\n..#.#...#.#\n.#...##..#.\n..#.##.....\n.#.#.#....#\n.#........#\n#.##...#...\n#...##....#\n.#..#...#.#");
 
     CHECK_MESSAGE(-1 == day.process2(input), "test process 2");
 }
