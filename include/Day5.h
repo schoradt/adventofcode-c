c860f3dcf3a32bc8e58116aab6748379b0909380
Sven Schoradt <sven.schoradt@googlemail.com>
2020-12-05 23:47:53 +0100

    FIX: refactor the reduction algorithm

--- include/Day5.h
+++ include/Day5.h
@@ -10,6 +10,8 @@ class Day5: public Base
 {
 private:
     // private vars
+    int computeReduction(string rules, int low, int high, char lower, char upper);
+
 public:
     Day5(/* args */);
 
--- src/Day5.cpp
+++ src/Day5.cpp
@@ -11,63 +11,26 @@ Day5::Day5() {
 
 }
 
+int Day5::computeReduction(string rules, int low, int high, char lower, char upper) {
+    for (size_t i = 0; i < rules.size(); i++) {
+        char c = rules.at(i);
+
+        if (c == lower) {
+            high = low + ((high - low + 1) / 2) - 1;
+        } else if (c == upper) {
+            low = low + (high - low + 1) / 2;
+        }
+    }
+
+    return low;
+}
+
 int Day5::computeSeatId(string boardingPass) {
-    int low = 0;
-    int high = 127;
 
-    for (int i = 0; i < 6; i++) {
-        char c = boardingPass.at(i);
+    int row = computeReduction(boardingPass.substr(0, 7), 0, 127, 'F', 'B');
 
-        if (c == 'F') {
-            high = low + ((high - low + 1) / 2) - 1;
-        } else if (c == 'B') {
-            low = low + (high - low + 1) / 2;
-        }
-
-        //cout << "HL - " << c << " : " << low << " - " << high << endl;
-    }
-
-    int row = 0;
-
-    char c = boardingPass.at(6);
-
-    if (c == 'F') {
-        row = low;
-    } else if (c == 'B') {
-        row = high;
-    }
-
-    //cout << "HL - " << c << " : " << low << " - " << high << endl;
-
-    low = 0;
-    high = 7;
-
-    for (int i = 7; i < 9; i++) {
-        char c = boardingPass.at(i);
-
-        if (c == 'L') {
-            high = low + ((high - low + 1) / 2) - 1;
-        } else if (c == 'R') {
-            low = low + (high - low + 1) / 2;
-        }
-
-        // cout << "HL - " << c << " : " << low << " - " << high << endl;
-    }
-
-    int seat = 0;
-
-    c = boardingPass.at(9);
-
-    if (c == 'L') {
-        seat = low;
-    } else if (c == 'R') {
-        seat = high;
-    }
-
-    //cout << "HL - " << c << " : " << low << " - " << high << endl;
-
-    // cout << row << " - " << seat;
-
+    int seat = computeReduction(boardingPass.substr(7, 3), 0, 7, 'L', 'R');
+    
     return row * 8 + seat;
 }
 
