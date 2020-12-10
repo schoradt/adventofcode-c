2cc85e1a2965455122324769fc8a4aeaa383d356
Sven Schoradt <sven.schoradt@googlemail.com>
2020-12-10 14:56:22 +0100

    FIX: day 10 part 2
    Tested 3 solutiions for part 2 and at the end it was solut 1 with
    caching.

--- src/Day10.cpp
+++ src/Day10.cpp
@@ -47,7 +47,46 @@ long Day10::process1(vector<int> input) {
     return step1 * step3;
 }
 
+long computeVariants3(vector<int>* sorted, map<size_t, long>* cache, size_t index) {
+    long vars = 0;
+
+    if ((*cache)[index]) {
+        return (*cache)[index];
+    }
+
+    size_t size = sorted->size();
+
+    if (index == size) {
+        return 1;
+    }
+
+    vars = vars + computeVariants3(sorted, cache, index + 1);
+
+    if (index + 2 < size && sorted->at(index + 2) - sorted->at(index) <= 3) {
+        vars = vars + computeVariants3(sorted, cache, index + 2);
+    }
+
+    if (index + 3 < size && sorted->at(index + 3) - sorted->at(index) <= 3) {
+        vars = vars + computeVariants3(sorted, cache, index + 3);
+    }
+
+    (*cache)[index] = vars;
+
+    return vars;
+}
+
+
 long Day10::process2(vector<int> input) {
-    
-    return -1;
+    //int max = *max_element(input.begin(), input.end());
+
+    vector<int> sorted = input;
+
+    sorted.push_back(0);
+
+    sort(sorted.begin(), sorted.end());
+
+    map<size_t, long> cache;
+
+    //return computeVariants2(&sorted, 0, 0, max);
+    return computeVariants3(&sorted, &cache, 0);
 }
--- test/Day10.cpp
+++ test/Day10.cpp
@@ -21,7 +21,11 @@ TEST_CASE("Testing Day 10 process 2")
 {
     Day10 day;
 
-    vector<int> input = day.parseIntegerLines(day.loadLinesString(""));
+    vector<int> input = day.parseIntegerLines(day.loadLinesString("16\n10\n15\n5\n1\n11\n7\n19\n6\n12\n4"));
 
-    CHECK_MESSAGE(-1 == day.process2(input), "test process 2");
+    CHECK_MESSAGE(8 == day.process2(input), "test process 2");
+
+    vector<int> input2 = day.parseIntegerLines(day.loadLinesString("28\n33\n18\n42\n31\n14\n46\n20\n48\n47\n24\n23\n49\n45\n19\n38\n39\n11\n1\n32\n25\n35\n8\n17\n7\n9\n4\n2\n34\n10\n3"));
+
+    CHECK_MESSAGE(19208 == day.process2(input2), "test process 2");
 }
